c1024c677271475c03c18827a71d1684d15eac69
zafar owais <zafarowais6663@gmail.com>
2025-01-23 11:56:52 +0530

    customize  war name

--- src/test/java/com/zafar/JenkinsCiCdAppApplicationTests.java
+++ src/test/java/com/zafar/JenkinsCiCdAppApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
 @WebMvcTest
 class JenkinsCiCdAppApplicationTests {
 
@@ -24,8 +23,5 @@ class JenkinsCiCdAppApplicationTests {
 
 	}
 
-	@Test
-	void contextLoads() {
-	}
 
 }
